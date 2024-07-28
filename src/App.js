e104622a570540c8f9b18821f91568a015524a83
AzamatOrtiqovdev <ortiqovazamat890@gmail.com>
2024-07-28 08:02:46 +0500

    rendering item

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Toothbrush", quantity: 6, packed: true },
+  { id: 4, description: "Toothpaste", quantity: 1, packed: false },
+
+];
+
 export default function App() {
   return (
     <div>
@@ -18,7 +26,22 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return <div className="list">
+    <ul>
+      {initialItems.map((item) => <Item item={item}/>)}
+    </ul>
+  </div>;
+}
+
+function Item({item}) {
+  return (
+    <li>
+      <span style={item.packed ? {textDecoration: "line-through"} : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  )
 }
 
 function Stats() {
